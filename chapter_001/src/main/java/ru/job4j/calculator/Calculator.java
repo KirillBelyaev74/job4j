e8bfadaec2fd83345d343757402a00a6bfcb48c7
KirillBelyaev74 <kirbel74@gmail.com>
2019-08-23 17:24:10 +0500

    5.7. JavaDoc. Code convention[#169564]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,53 @@
+/**
+ * Calculator - Класс для орефметических операций
+ *
+ * @author Kirill Belayev
+ * @version 23.08.19
+ * @since 1.0
+ */
 package ru.job4j.calculator;
 
 public class Calculator {
-
+    /**
+     * Метод для сложения чисел
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
+    /**
+     * Метод для деления чисел
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
+    /**
+     * Метод для умножения чисел
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
-    }
+    }    /**
+     * Метод для вычитания чисел
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void subtrack(double first, double second) {
-        double result =  first - second;
+        double result = first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
 
+    /**
+     * main
+     * @param args
+     */
     public static void main(String[] args) {
         add(1, 1);
         subtrack(10, 5);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * package - для расчета задачь
+ *
+ * @author Kirill Belayev
+ * @version 23.08.19
+ * @since 1.0
+ */
+package ru.job4j.calculator;
