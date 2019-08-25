bf8a11afe050aeea7036f710f66097c2fb126daa
KirillBelyaev74 <kirbel74@gmail.com>
2019-08-25 18:29:35 +0500

    6.1. Классический поиск перебором.[#169571]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -22,4 +22,13 @@ public class FindLoopTest {
         int expect = 2;
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenArrayHas8Then1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {11, 6, 2};
+        int value = 8;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
