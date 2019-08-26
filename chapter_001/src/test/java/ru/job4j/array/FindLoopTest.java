404493f16752cc2c107316412df948f9f79f86e9
KirillBelyaev74 <kirbel74@gmail.com>
2019-08-26 12:44:16 +0500

    6.5. Поиск индекса в диапазоне.[#169582]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
     @Test
-    public void whenArrayHas5Then0() {
+    public void whenArrayHasLengh5Then0() {
         FindLoop find = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 5;
@@ -14,20 +14,35 @@ public class FindLoopTest {
         assertThat(result, is(expect));
     }
     @Test
-    public void whenArrayHas2Then2() {
+    public void whenFind3() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {4, 7, 9};
-        int value = 9;
-        int result = find.indexOf(input, value);
-        int expect = 2;
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
     @Test
-    public void whenArrayHas8Then1() {
+    public void whenFind5() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {11, 6, 2};
-        int value = 8;
-        int result = find.indexOf(input, value);
+        int[] input = new int[] {5, 2, 10, 2, 4, 7, 3, 6};
+        int value = 7;
+        int start = 4;
+        int finish = 6;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = 5;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFind0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 7;
+        int start = 1;
+        int finish = 3;
+        int result = find.indexOf(input, value, start, finish);
         int expect = -1;
         assertThat(result, is(expect));
     }
