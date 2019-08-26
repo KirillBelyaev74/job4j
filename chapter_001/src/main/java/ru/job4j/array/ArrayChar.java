602b4b7b76b9c354ccdc9183574e97a586882143
KirillBelyaev74 <kirbel74@gmail.com>
2019-08-26 12:15:09 +0500

    6.4. Слово начинается с ...[#169573]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public boolean startsWith(String word, String prefix) {
+        boolean result = true;
+        char[] pref = prefix.toCharArray();
+        char[] wrd = word.toCharArray();
+       // result = word.startsWith(prefix);
+        for(int index = 0; index < pref.length;  index++) {
+            if(pref[index] != wrd[index]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
