ee7ebde727d5fb808763c7b8b2fd7f88ee5d0a8c
KirillBelyaev74 <kirbel74@gmail.com>
2019-09-11 17:27:04 +0500

    4. Слияние веток[#169652]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Student {
+
+    public void song() {
+        System.out.print("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+}
