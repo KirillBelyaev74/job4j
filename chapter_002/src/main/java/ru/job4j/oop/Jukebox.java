f66f64d34a45531bef324b85dc8a6f854f403d21
KirillBelyaev74 <kirbel74@gmail.com>
2019-09-11 17:40:30 +0500

    1.3. Музыкальный автомат[#169690]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        Jukebox jukebox = new Jukebox();
+        jukebox.music(one);
+        jukebox.music(two);
+        jukebox.music(3);
+    }
+}
