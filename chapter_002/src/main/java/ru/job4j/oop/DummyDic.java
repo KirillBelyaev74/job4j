41e5d84e106bf67fa94e553bf93545363159bcb1
KirillBelyaev74 <kirbel74@gmail.com>
2019-09-11 17:50:18 +0500

    1.4. Переводчик Русский-Английский.[#169692]

---
+++ chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+    public String engToRus(String eng) {
+        String input = "Неизвестное слово: " + eng;
+        return input;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dummyDic = new DummyDic();
+        String result = dummyDic.engToRus("World");
+        System.out.println(result);
+    }
+}
