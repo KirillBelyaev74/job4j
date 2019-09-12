da1b833edb783508bcabdf158aeccc2d0eaf0c3d
KirillBelyaev74 <kirbel74@gmail.com>
2019-09-12 16:37:05 +0500

    2.1. Рефакторинг - Расстояние между точками.[#169682]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,14 +1,21 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
 
-    public static void main(String[] args) {
-        double resultOne = distance(0, 0, 2, 0);
-        double resultTwo = distance(3, 5, 7, 2);
-        System.out.println("result (0, 0) to (2, 0) " + resultOne);
-        System.out.println("result (3, 5) to (7, 2) " + resultTwo);
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
+
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,18 +1,32 @@
 package ru.job4j.condition;
-import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class PointTest {
     @Test
-    public void distanceOne() {
-        int expected = 2;
-        int out = (int) Point.distance(0, 0, 2, 0);
-        Assert.assertEquals(expected, out);
+    public void whenZeroAndTenThenTen() {
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 10);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(10D));
     }
+
     @Test
-    public void distanceTwo() {
-        int expected = 5;
-        int out = (int) Point.distance(3, 5, 7, 2);
-        Assert.assertEquals(expected, out);
+    public void whenCheckItself() {
+        Point point = new Point(0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
     }
 }
