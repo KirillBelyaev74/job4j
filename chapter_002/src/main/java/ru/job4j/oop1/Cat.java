98962bf89b9b95bb1d18910af158246cab2437cc
KirillBelyaev74 <kirbel74@gmail.com>
2019-09-20 13:02:24 +0500

    1. Классы и объекты[#169681]

--- chapter_002/src/main/java/ru/job4j/oop1/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop1/Cat.java
@@ -1,4 +1,4 @@
-package ru.job4j.oop;
+package ru.job4j.oop1;
 
 public class Cat {
 
--- chapter_002/src/main/java/ru/job4j/oop1/Dog.java
+++ chapter_002/src/main/java/ru/job4j/oop1/Dog.java
@@ -1,4 +1,4 @@
-package ru.job4j.oop;
+package ru.job4j.oop1;
 
 public class Dog {
     public static void main (String[] args) {
