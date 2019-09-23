076ef6473523ba2627b19ce3cf9921d4723a74b3
KirillBelyaev74 <kirbel74@gmail.com>
2019-09-23 19:26:38 +0500

    Экзамен. Базовый синтаксис.[#169606]

--- chapter_001/src/main/java/ru/job4j/array/ArrayMax.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayMax.java
@@ -13,22 +13,11 @@ public class ArrayMax {
         int two = 0;
         int[] result = new int[inputOne.length + inputTwo.length];
         while (x < result.length) {
-            if (one == inputOne.length  && two < inputTwo.length) {
-                result[x++] = inputTwo[two++];
-            } else if (two == inputTwo.length && one < inputOne.length) {
+            if (one != inputOne.length && two != inputTwo.length) {
+                result[x++] = inputOne[one] < inputTwo[two] ? inputOne[one++] : inputTwo[two++];
+            } else if (one != inputOne.length) {
                 result[x++] = inputOne[one++];
-            } else if (inputOne[one] < inputTwo[two]) {
-                result[x++] = inputOne[one];
-                if (one < inputOne.length) {
-                    one++;
-                }
-            } else if (inputTwo[two] < inputOne[one]) {
-                result[x++] = inputTwo[two];
-                if (two < inputTwo.length) {
-                    two++;
-                }
-            } else if (inputOne[one] == inputTwo[two]) {
-                result[x++] = inputOne[one++];
+            } else {
                 result[x++] = inputTwo[two++];
             }
         }
