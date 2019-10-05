4916efeb3c619ea0ada16affc56f91f2105389ec
KirillBelyaev74 <kirbel74@gmail.com>
2019-10-05 13:07:38 +0500

    5. Рефакторинг теста @Before @After[#169665]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,31 +1,60 @@
 package ru.job4j.pseudo;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.stragery.Paint;
 import ru.job4j.stragery.Square;
+import ru.job4j.stragery.Triangle;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
                         new StringBuilder()
                                 .append("+++++++\n")
                                 .append("+     +\n")
                                 .append("+     +\n")
-                                .append("+++++++\n")
+                                .append("+++++++")
                                 .append(System.lineSeparator())
                                 .toString()
                 )
         );
-        System.setOut(stdout);
+    }
+    @Test
+    public void whenDrawTriangle() {
+        Triangle square = new Triangle();
+        assertThat(
+                square.draw(),
+                is(
+                        new StringBuilder()
+                                .append("  ^  \n")
+                                .append(" ^ ^ \n")
+                                .append("^^^^^")
+                                .toString()
+                )
+        );
     }
 }
