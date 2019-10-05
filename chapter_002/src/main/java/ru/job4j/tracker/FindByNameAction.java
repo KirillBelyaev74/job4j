5f490fb49f4f7f34913243e3f877eb8b710d5750
KirillBelyaev74 <kirbel74@gmail.com>
2019-10-05 13:58:12 +0500

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#169666]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker;
+
+public class FindByNameAction implements UserAction {
+    private String key;
+    public FindByNameAction (String key) {
+        this.key = key;
+    }
+    public boolean execute(Input input, Tracker tracker){
+        for (Item items : tracker.findByName(key)) {
+            System.out.println(String.format("%s %s", items.getId(), items.getName()));
+        }
+        return true;
+    }
+    public String name() {
+        return "Find by name.";
+    }
+}
