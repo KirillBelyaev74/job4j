b1bb1fdf2445e2ce826dda01c6c5cd4f95aff549
KirillBelyaev74 <kirbel74@gmail.com>
2019-11-30 12:27:14 +0500

    Каркас шахматной доски[#169709]

---
+++ chapter_002/src/main/java/ru/job4j/oop4/College.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop4;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Student sfreshman = freshman;
+        Object ofreshman = freshman;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop4/Student.java
@@ -0,0 +1,4 @@
+package ru.job4j.oop4;
+
+public class Student extends Object {
+}
