fb61268ef863a911ddbaf91bf9940c95d423e4e5
KirillBelyaev74 <kirbel74@gmail.com>
2020-02-07 12:28:02 +0500

    1. Подсчет функции в диапазоне.[#169517]

---
+++ chapter_004/src/test/java/ru/job4j/diapason/TestDiapason.java
@@ -0,0 +1,36 @@
+package ru.job4j.diapason;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class TestDiapason {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenLinearFunctionThenSquareResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> x * x);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenLinearFunctionThenLogResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> Math.log(x));
+        List<Double> expected = Arrays.asList(0.02D, 0.02D, 0.02D);
+        for(int index = 0; index != result.size(); index++) {
+            assertThat(((double)(int)Math.round(result.get(index))/ 100), is(expected.get(index)));
+        }
+    }
+}
