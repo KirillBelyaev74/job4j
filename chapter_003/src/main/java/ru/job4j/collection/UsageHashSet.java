cc225dd8fce797cc943d30ba376aa398f78d2ade
KirillBelyaev74 <kirbel74@gmail.com>
2020-02-26 18:05:44 +0500

    1. Collection API Улучшения[#169532]

--- chapter_003/src/main/java/ru/job4j/collection/UsageHashSet.java
+++ chapter_003/src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -1,14 +1,10 @@
 package ru.job4j.collection;
 
-import java.util.HashSet;
+import java.util.Set;
 
 public class UsageHashSet {
     public static void main(String[] args) {
-        HashSet<String> autos = new HashSet<String>();
-        autos.add("Lada");
-        autos.add("BMW");
-        autos.add("Volvo");
-        autos.add("Toyota");
+        Set<String> autos = Set.of("Lada" ,"BMW" ,"Volvo" ,"Toyota");
         for (String auto : autos) {
             System.out.println(auto);
         }
