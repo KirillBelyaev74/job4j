4cd38373ffd80a82006a9bc16c9334b658435d69
KirillBelyaev74 <kirbel74@gmail.com>
2020-03-05 10:53:15 +0500

    Контрольные вопросы.[#169636]

---
+++ chapter_003/src/main/java/ru/job4j/departments/Departments.java
@@ -0,0 +1,38 @@
+package ru.job4j.departments;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        HashSet<String> tmp = new HashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                 tmp.add(start + el);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static List<String> sortAsc(List<String> orgs) {
+        List<String> result = Departments.addString(Departments.fillGaps(orgs));
+        Collections.sort(result);
+        return result;
+    }
+
+    public static List<String> sortDesc(List<String> orgs) {
+        List<String> result = Departments.addString(Departments.fillGaps(orgs));
+        result.sort(new DepDescComp());
+        return result;
+    }
+
+    public static List<String> addString(List<String> orgs) {
+        List<String> result = new ArrayList<>(orgs);
+        Collections.sort(result);
+        for(int index = 1; index != result.size(); index++) {
+            result.set(index, result.get(index - 1) + "/" + result.get(index));
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/departments/DepartmentsTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.departments;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DepartmentsTest {
+    @Test
+    public void whenMissedSortAsc() {
+        List<String> input = Arrays.asList("k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.sortAsc(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChangeOneSortAsc() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.sortAsc(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChangeTwoSortAsc() {
+        List<String> input = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> result = Departments.sortAsc(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenMissedSortDesc() {
+        List<String> input = Arrays.asList("k1/sk1");
+        List<String> expect = Arrays.asList("k1/sk1", "k1");
+        List<String> result = Departments.sortDesc(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChangeOneSortDesc() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1/sk1", "k1");
+        List<String> result = Departments.sortDesc(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChangeTwoSortDesc() {
+        List<String> input = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> expect = Arrays.asList("k1/sk1/ssk1", "k1/sk1", "k1");
+        List<String> result = Departments.sortDesc(input);
+        assertThat(result, is(expect));
+    }
+}
