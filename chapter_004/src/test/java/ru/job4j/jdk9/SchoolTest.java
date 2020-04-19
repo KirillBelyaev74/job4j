a641e4a261e659c1188818369f251e04a6b35323
KirillBelyaev74 <kirbel74@gmail.com>
2020-04-19 10:28:47 +0500

    Validate

--- chapter_004/src/test/java/ru/job4j/jdk9/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/jdk9/SchoolTest.java
@@ -13,18 +13,18 @@ public class SchoolTest {
     public void whenMore70() {
         School school = new School();
         List<Student> students = List.of(
-                new Student("Ivan", 90)
-                ,new Student("Kostya", 10)
-                ,new Student("Petr", 70)
-                ,new Student("Kirill", 30)
-                ,new Student("Alex", 50)
-                ,new Student(null, 0)
+                new Student("Ivan", 90),
+                new Student("Kostya", 10),
+                new Student("Petr", 70),
+                new Student("Kirill", 30),
+                new Student("Alex", 50),
+                new Student(null, 0)
         );
         List<Student> result = school.levelOf(students, 50);
         List<Student> expect = List.of(
-                new Student("Ivan", 90)
-                ,new Student("Petr", 70)
-                ,new Student("Alex", 50)
+                new Student("Ivan", 90),
+                new Student("Petr", 70),
+                new Student("Alex", 50)
         );
         assertThat(expect, is(result));
     }
